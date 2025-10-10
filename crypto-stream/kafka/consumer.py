554dcb62968b1f699868b6d87fe6a2c96848eb47
Anh Tu <sweantu@gmail.com>
2025-10-10 17:50:56 +0700

    feat: update kafka image

--- crypto-stream/kafka/consumer.py
+++ crypto-stream/kafka/consumer.py
@@ -22,7 +22,11 @@ try:
         if msg.error():
             raise KafkaException(msg.error())
         data = json.loads(msg.value().decode("utf-8"))
-        print(f"📩 Received: {data}")
+        print(
+            f"📩 Received from topic '{msg.topic()}' "
+            f"partition {msg.partition()} "
+            f"offset {msg.offset()}:\n{data}\n"
+        )
 except KeyboardInterrupt:
     pass
 finally:
