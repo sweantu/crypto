43c285ac42785257f8f3c24a7e27096d7c54fdb5
Anh Tu <sweantu@gmail.com>
2025-10-14 16:45:52 +0700

    feat: test_transform_job_pattern_two.py

--- crypto-stream/jupyter/workspace/test_transform_job_pattern_two.py
+++ crypto-stream/jupyter/workspace/test_transform_job_pattern_two.py
@@ -9,10 +9,9 @@ from pyflink.table import EnvironmentSettings, StreamTableEnvironment
 
 env = StreamExecutionEnvironment.get_execution_environment()
 env.set_parallelism(1)
-
-
 settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
 t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
 
 t_env.execute_sql("DROP TABLE IF EXISTS klines_source")
 t_env.execute_sql("""
@@ -32,10 +31,11 @@ CREATE TABLE klines_source (
 """)
 
 
-input_stream = t_env.to_data_stream(t_env.from_path("klines_source")).map(
-    lambda r: Row("ADAUSDT", int(r[0].timestamp()), float(r[5])),
-    output_type=Types.ROW([Types.STRING(), Types.INT(), Types.DOUBLE()]),
-)
+def round_half_up(x, decimals=2):
+    if x is None:
+        return None
+    factor = 10**decimals
+    return float(int(x * factor + 0.5)) / factor
 
 
 class EMA7Function(KeyedProcessFunction):
@@ -47,41 +47,71 @@ class EMA7Function(KeyedProcessFunction):
         self.alpha = 2 / (7 + 1)
 
     def process_element(self, value, ctx):
-        symbol, ts, close_price = value
         prev_ema = self.ema_state.value()
 
         if prev_ema is None:
-            self.buffer.append(close_price)
+            self.buffer.append(value["close_price"])
             if len(self.buffer) < 7:
+                yield Row(**value.as_dict(), ema7=None)
                 return
             ema = sum(self.buffer) / len(self.buffer)
             self.buffer.clear()
         else:
-            ema = self.alpha * close_price + (1 - self.alpha) * prev_ema
+            ema = self.alpha * value["close_price"] + (1 - self.alpha) * prev_ema
 
         self.ema_state.update(ema)
-        yield Row(symbol, ts, close_price, ema)
+        yield Row(**value.as_dict(), ema7=round_half_up(ema, 4))
 
 
-ema_stream = input_stream.key_by(lambda x: x[0]).process(
-    EMA7Function(),
-    output_type=Types.ROW(
-        [Types.STRING(), Types.INT(), Types.DOUBLE(), Types.DOUBLE()]
-    ),
+klines_stream = t_env.to_data_stream(t_env.from_path("klines_source")).map(
+    lambda r: Row(**r.as_dict(), symbol="ADAUSDT")
+)
+ema7_typeinfo = Types.ROW_NAMED(
+    [
+        "window_start",
+        "window_end",
+        "open_price",
+        "high_price",
+        "low_price",
+        "close_price",
+        "volume",
+        "ema7",
+        "symbol",
+    ],
+    [
+        Types.SQL_TIMESTAMP(),
+        Types.SQL_TIMESTAMP(),
+        Types.DOUBLE(),
+        Types.DOUBLE(),
+        Types.DOUBLE(),
+        Types.DOUBLE(),
+        Types.DOUBLE(),
+        Types.DOUBLE(),
+        Types.STRING(),
+    ],
+)
+ema_stream = klines_stream.key_by(lambda x: x["symbol"]).process(
+    EMA7Function(), output_type=ema7_typeinfo
 )
 
 
 t_env.execute_sql("DROP TABLE IF EXISTS ema7_sink")
 t_env.execute_sql("""
 CREATE TABLE ema7_sink (
-    symbol STRING,
-    ts INT,
+    window_start TIMESTAMP(3),
+    window_end TIMESTAMP(3),
+    open_price DOUBLE,
+    high_price DOUBLE,
+    low_price DOUBLE,
     close_price DOUBLE,
-    ema7 DOUBLE
+    volume DOUBLE,
+    ema7 DOUBLE,
+    symbol STRING
 ) WITH (
     'connector' = 'filesystem',
     'path' = '/workspace/output/ema7',
-    'format' = 'csv'
+    'format' = 'csv',
+     'csv.null-literal' = ''
 )
 """)
 
