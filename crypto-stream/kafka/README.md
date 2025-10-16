568aaedd0da4c6950cb1a360eaea8a228f5b9557
Anh Tu <sweantu@gmail.com>
2025-10-16 12:07:34 +0700

    feat: kafka producer

--- crypto-stream/kafka/README.md
+++ crypto-stream/kafka/README.md
@@ -5,6 +5,8 @@ docker exec kafka kafka-topics --create \
  --bootstrap-server localhost:9092 \
  --partitions 1 --replication-factor 1
 
+kafka-topics --bootstrap-server localhost:9092 --create --topic aggtrades-topic --partitions 3 --replication-factor 1
+
 # Produce a message
 
 docker exec -it kafka kafka-console-producer \
@@ -14,3 +16,16 @@ docker exec -it kafka kafka-console-producer \
 
 docker exec -it kafka kafka-console-consumer \
  --topic test-topic --from-beginning --bootstrap-server localhost:9092
+
+
+# check partitions
+kafka-topics --bootstrap-server localhost:9092 --list
+kafka-topics --bootstrap-server localhost:9092 --topic aggtrades-topic --describe
+
+# Consume from partition 0
+kafka-console-consumer --bootstrap-server localhost:9092 \
+--topic aggtrades-topic --partition 0 --from-beginning --max-messages 5
+
+# Consume from partition 1
+kafka-console-consumer --bootstrap-server localhost:9092 \
+--topic aggtrades-topic --partition 1 --from-beginning --max-messages 5
--- crypto-stream/kafka/producer.py
+++ crypto-stream/kafka/producer.py
@@ -1,24 +1,24 @@
 import csv
 import json
+import subprocess
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 from confluent_kafka import Producer
-
-conf = {"bootstrap.servers": "localhost:29092"}
-producer = Producer(conf)
-topic = "aggtrades-topic"
 
 
 def delivery_report(err, msg):
     if err is not None:
         print(f"❌ Delivery failed: {err}")
     else:
-        print(
-            f"✅ Delivered to {msg.topic()} [{msg.partition()}] offset {msg.offset()}"
-        )
+        # print(
+        #     f"✅ Delivered to {msg.topic()} [{msg.partition()}] offset {msg.offset()}"
+        # )
+        pass
 
 
 class AggTrade:
-    def __init__(self, row: list[str]) -> None:
+    def __init__(self, row: list[str], symbol: str) -> None:
         self.agg_trade_id = int(row[0])
         self.price = float(row[1])
         self.quantity = float(row[2])
@@ -27,23 +27,68 @@ class AggTrade:
         self.ts_int = int(row[5])
         self.is_buyer_maker = row[6].strip().lower() == "true"
         self.is_best_match = row[7].strip().lower() == "true"
+        self.symbol = symbol
+
+def produce_messages(topic: str, symbol: str, file_path: str, conf: dict) -> None:
+    symbol_to_partition = {"ADAUSDT": 0, "BTCUSDT": 1, "ETHUSDT": 2}
+    producer = Producer(conf)
+    num_lines = int(subprocess.check_output(["wc", "-l", file_path]).split()[0])
+    duration_in_seconds = 90
+    t_start = time.time()
+    with open(
+        file_path,
+        newline="",
+        encoding="utf-8",
+    ) as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader, 1):
+            try:
+                msg = json.dumps(AggTrade(row, symbol).__dict__)
+                producer.produce(
+                    topic,
+                    partition=symbol_to_partition[symbol],
+                    key=symbol.encode("utf-8"),
+                    value=msg.encode("utf-8"),
+                    callback=delivery_report,
+                )
+                if i % int(num_lines / duration_in_seconds) == 0:
+                    producer.poll(0)
+                    print(
+                        f"Produced {i} messages for {symbol} partition {symbol_to_partition[symbol]} so far..."
+                    )
+                    time.sleep(1)
+            except Exception as e:
+                print(f"❌ Exception while producing message: {i}, error: {e}")
+
+    producer.flush()
+    t_end = time.time()
+    print(f"Time taken to produce {num_lines} messages: {t_end - t_start:.2f} seconds")
+    print("All messages have been produced.")
 
 
-with open(
-    "../jupyter/workspace/ADAUSDT-aggTrades-2025-09-27.csv",
-    newline="",
-    encoding="utf-8",
-) as f:
-    reader = csv.reader(f)
-    for i, row in enumerate(reader, 1):
-        try:
-            msg = json.dumps(AggTrade(row).__dict__)
-            producer.produce(topic, value=msg.encode("utf-8"), callback=delivery_report)
-            if i % 1000 == 0:
-                producer.poll(0)
-                print(f"Produced {i} messages so far...")
-        except Exception as e:
-            print(f"❌ Exception while producing message: {i}, error: {e}")
-
-producer.flush()
-print("All messages have been produced.")
+if __name__ == "__main__":
+    conf = {"bootstrap.servers": "localhost:29092"}
+    topic = "aggtrades-topic"
+    symbols = ["ADAUSDT", "BTCUSDT", "ETHUSDT"]
+    t_start_all = time.time()
+    with ThreadPoolExecutor() as executor:
+        for i in range(3):
+            t_start_round = time.time()
+            file_paths = [
+                f"../jupyter/workspace/input/{symbol}-aggTrades-2025-09-{26 + i}.csv"
+                for symbol in symbols
+            ]
+            futures = [
+                executor.submit(produce_messages, topic, symbol, file_path, conf)
+                for symbol, file_path in zip(symbols, file_paths)
+            ]
+            for future in futures:
+                future.result()
+            t_end_round = time.time()
+            print(
+                f"Time taken for round {i + 1}: {t_end_round - t_start_round:.2f} seconds"
+            )
+            print(f"Completed round {i + 1}/3")
+    t_end_all = time.time()
+    print(f"Total time taken: {t_end_all - t_start_all:.2f} seconds")
+    print("All rounds completed.")
